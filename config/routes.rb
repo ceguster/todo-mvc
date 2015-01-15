0bd7d6ee719cc329201dcf34bf697d75a0198c65
ceguster <cguster@gmail.com>
2015-01-15 16:26:56 -0500

    resources for boards, lists, items and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :boards
+  resources :lists do
+    resources :items
+  end
+
+  root to: 'boards#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
