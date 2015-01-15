23d520e020767d8f75ea259daded9ac3b22ade22
ceguster <cguster@gmail.com>
2015-01-15 18:05:12 -0500

    start seed data, board controller actions, basic board index view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,26 @@ class BoardsController < ApplicationController
     @boards = Board.all
     @board = Board.new
   end
+
+  def create
+    @board = Board.create(board_params)
+    redirect_to board_path(@boarda)
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    @board.update(board_params)
+    @board.save
+
+    redirect_to @board
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#boards
+boards = Board.create([{name: 'Cetera'}, {name: 'ShindigIt'}])
+r
